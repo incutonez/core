581d3b77d03f28f48d4856527879f096e336f1ed
Jef <incutonez@hotmail.com>
2022-02-10 21:32:23 -0500

    - Using absolute paths

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import postcss from "./postcss.config.js";
+import postcss from "ui/postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
