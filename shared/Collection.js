59e5f09a83a6d597f45f577142c5865a5b1e6869
Jef <incutonez@hotmail.com>
2022-02-12 18:57:02 -0500

    - Adding addFilters method

--- shared/Collection.js
+++ shared/Collection.js
@@ -6,6 +6,8 @@ import { Model } from "shared/Model.js";
 
 export class Collection extends Array {
   isCollection = true;
+  filters = [];
+  _dataSource = null;
 
   constructor(data, model = Model) {
     super();
@@ -28,16 +30,23 @@ export class Collection extends Array {
   }
 
   add(data, clear = false) {
-    if (isEmpty(data)) {
-      return;
-    }
     if (clear) {
       this.clear();
+    }
+    if (isEmpty(data)) {
+      return;
     }
     if (!isArray(data)) {
       data = [data];
     }
-    data.map((item) => this.push(new this.model(item)));
+    data.map((item) => {
+      if (item.isModel) {
+        this.push(item);
+      }
+      else {
+        this.push(new this.model(item));
+      }
+    });
   }
 
   getData(options) {
@@ -48,5 +57,53 @@ export class Collection extends Array {
 
   clone() {
     return new this.constructor(this.getData(), this.model);
+  }
+
+  clearFilters() {
+    this.filters = [];
+    this.add(this._dataSource, true);
+  }
+
+  /**
+   * @typedef CollectionFilter
+   * @property {String} name
+   * @property {String} property
+   * @property {*} value
+   * @property {Function} fn
+   */
+  /**
+   * @param {CollectionFilter|CollectionFilter[]} filters
+   */
+  addFilters(filters) {
+    if (isEmpty(filters)) {
+      return;
+    }
+    // We're adding filters for the very first time, so let's set up a pointer to our base data
+    if (isEmpty(this.filters)) {
+      this._dataSource = this.slice();
+    }
+    if (!isArray(filters)) {
+      filters = [filters];
+    }
+    const data = [];
+    this.filters = this.filters.concat(filters);
+    filters.forEach((filter) => {
+      if (filter.fn) {
+        this.forEach((record) => {
+          if (filter.fn(record)) {
+            data.push(record);
+          }
+        });
+      }
+      else {
+        const { property, value } = filter;
+        this.forEach((record) => {
+          if (record[property] === value) {
+            data.push(record);
+          }
+        });
+      }
+    });
+    this.add(data, true);
   }
 }
