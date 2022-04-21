9231b3a292d8d39ed407d414545097b75cb1b861
Jef <incutonez@hotmail.com>
2022-04-21 19:27:31 -0400

    - Adding cell and copy cursors

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -8,6 +8,10 @@ export default {
       "16": "4rem",
     },
     extend: {
+      cursor: {
+        cell: "cell",
+        copy: "copy",
+      },
       boxShadow: {
         top: "0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
       },
