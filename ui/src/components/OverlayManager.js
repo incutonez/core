89a04799a450c66aff7d13b6849fcfadc5ea0740
Jef <incutonez@hotmail.com>
2022-04-21 12:14:48 -0400

    - Adding comments - Minor style changes - Adding build UI script

--- ui/src/components/OverlayManager.js
+++ ui/src/components/OverlayManager.js
@@ -1,7 +1,19 @@
-﻿export class OverlayManager extends HTMLElement {
+﻿/**
+ * It's good to note why I'm not using Shadow DOM here, and that's mainly because Tailwind CSS isn't easy to
+ * propagate down to shadow components without doing a full import of Tailwind.  Even then, the purging
+ * mechanism isn't robust enough to deal with this, so because this component is fairly lightweight and
+ * essentially just a wrapper/replacement for Teleport, we can get away with doing this.  If we had a larger
+ * use case (slots, etc.) or very seriously needed to use this as a WebComponent in other projects, this
+ * should be reworked.  We also don't define a Vue WebComponent
+ * Resources:
+ * - https://github.com/tailwindlabs/tailwindcss/discussions/1935
+ * - https://stackoverflow.com/a/71933848/1253609
+ * - https://vuejs.org/guide/extras/web-components.html#building-custom-elements-with-vue
+ * - https://forum.vuejs.org/t/how-to-access-webcomponent-methods-using-vue-syntax/129033
+ */
+export class OverlayManager extends HTMLElement {
   constructor() {
     super();
-    this.classList.add("absolute", "top-0", "left-0", "contents", "pointer-events-none", "z-10", "contain-layout");
     document.body.appendChild(this);
   }
 
