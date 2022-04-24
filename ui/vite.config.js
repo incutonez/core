bb1783b1a2ba74af6717d71d67d2a338c2c6f4c1
Jef <incutonez@hotmail.com>
2022-04-24 17:01:40 -0400

    - Testing

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,39 +5,54 @@ import postcss from "./postcss.config.js";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), cssInjectedByJsPlugin()],
-  css: {
-    postcss,
-    preprocessorOptions: {
-      scss: {
-        charset: false,
+export default defineConfig(({ mode }) => {
+  const baseConfig = {
+    plugins: [vue(), cssInjectedByJsPlugin()],
+    css: {
+      postcss,
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "ui": path.resolve(path.resolve(), "./src"),
-      "shared": path.resolve(path.resolve(), "../shared"),
-    },
-  },
-  build: {
-    lib: {
-      entry: "src/index.js",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["vue", "vue-router"],
-      input: {
-        "CoreUI": "src/core.js",
-        "index": "src/index.js",
-        "tailwind.config": "tailwind.config.js",
-      },
-      output: {
-        dir: "dist",
-        format: "es",
-        entryFileNames: "[name].js",
+    resolve: {
+      alias: {
+        "ui": path.resolve(path.resolve(), "./src"),
+        "shared": path.resolve(path.resolve(), "../shared"),
       },
     },
-  },
+  };
+  if (mode === "GitHubPages") {
+    baseConfig.build = {
+      rollupOptions: {
+        output: {
+          dir: "docs",
+        },
+      },
+    };
+  }
+  else {
+    baseConfig.build = {
+      lib: {
+        entry: "src/index.js",
+        formats: ["es"],
+      },
+      rollupOptions: {
+        external: ["vue", "vue-router"],
+        input: {
+          "CoreUI": "src/core.js",
+          "index": "src/index.js",
+          "tailwind.config": "tailwind.config.js",
+        },
+        output: {
+          dir: "dist",
+          format: "es",
+          entryFileNames: "[name].js",
+        },
+      },
+    };
+  }
+  baseConfig.build.emptyOutDir = false;
+  return baseConfig;
 });
